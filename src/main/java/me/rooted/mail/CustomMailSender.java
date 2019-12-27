236a0720fa05d000afe9cac9a5dbf13145e4880d
윤성근 <seonggn.yun@kt.com>
2019-12-27 15:57:35 +0900

    #1 기본 메일 전송 기능

---
+++ src/main/java/me/rooted/mail/CustomMailSender.java
@@ -0,0 +1,44 @@
+package me.rooted.mail;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+@Component
+public class CustomMailSender {
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
+	@Autowired
+	private SpringTemplateEngine templateEngine;
+	
+	public void sendMail() throws MessagingException, IOException {
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+		   
+        //메일 제목 설정
+        helper.setSubject("스프링 부트 메일 전송");
+        //수신자 설정
+        helper.setTo("seonggn.yun@gmail.com");
+        //템플릿에 전달할 데이터 설정
+        Context context = new Context();
+        context.setVariable("test_key", "test_value");
+        //메일 내용 설정 : 템플릿 프로세스
+        String html = templateEngine.process("mail-template",context);
+        helper.setText(html, true);
+        
+        //메일 보내기
+        javaMailSender.send(message);
+	}
+}
